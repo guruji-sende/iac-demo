97323135cd9bfd3b4d48e32f514b60074294ef7f
guruji-sende <120446636+guruji-sende@users.noreply.github.com>
2022-12-14 17:00:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-southeast-1"
-  access_key = "AKIATEC7SOBAG7KAA7RH"
-  secret_key = "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"
+  access_key = "AKIATEC7BAG7KAA7RHsds"
+  secret_key = "O8TRv54oJdasdWEqlej97sjJxOgX3TwzkYkfhYV"
 }
